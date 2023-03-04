2446f8964708d89c2e1adbc6d02f7075e21bf47c
Abuzar-cpu <kerimovabuzer25@gmail.com>
2023-03-04 12:15:33 +0400

    refactor

--- gatewayserver/src/main/java/az/yelo/gatewayserver/ApiGatewayServerApplication.java
+++ gatewayserver/src/main/java/az/yelo/gatewayserver/ApiGatewayServerApplication.java
@@ -3,6 +3,7 @@ package az.yelo.gatewayserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -14,5 +15,4 @@ public class ApiGatewayServerApplication {
   public static void main(String[] args) {
     SpringApplication.run(ApiGatewayServerApplication.class, args);
   }
-
 }
